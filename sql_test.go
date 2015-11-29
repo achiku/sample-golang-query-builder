529b3ada1ded219804dee423f1f536a3360ff627
Akira Chiku <akira.chiku@gmail.com>
2015-11-29 23:43:28 +0900

    Add retreaving data test

--- sql_test.go
+++ sql_test.go
@@ -47,6 +47,18 @@ func dropTable(db *sql.DB) error {
 	return nil
 }
 
+func insertData(db *sql.DB) error {
+	_, err := db.Exec(`
+	INSERT INTO account (name, dob) VALUES ('moqada', '1985/11/01');
+	INSERT INTO account (name, dob) VALUES ('8maki', '1985/04/01');
+	INSERT INTO account (name, dob) VALUES ('ideyuta', '1988/04/01');
+	`)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func TestPingDB(t *testing.T) {
 	db := createDB()
 	err := db.Ping()
@@ -61,6 +73,56 @@ func TestCreateDropTable(t *testing.T) {
 	if err != nil {
 		t.Errorf("failed to create table: %s", err)
 	}
+	err = dropTable(db)
+	if err != nil {
+		t.Errorf("failed to drop table: %s", err)
+	}
+}
+
+func TestInsertData(t *testing.T) {
+	db := createDB()
+	err := createTable(db)
+	if err != nil {
+		t.Errorf("failed to create table: %s", err)
+	}
+	err = insertData(db)
+	if err != nil {
+		t.Errorf("failed to insert data: %s", err)
+	}
+	err = dropTable(db)
+	if err != nil {
+		t.Errorf("failed to drop table: %s", err)
+	}
+}
+
+func TestSelectData(t *testing.T) {
+	db := createDB()
+	err := createTable(db)
+	if err != nil {
+		t.Errorf("failed to create table: %s", err)
+	}
+	err = insertData(db)
+	if err != nil {
+		t.Errorf("failed to insert data: %s", err)
+	}
+
+	var (
+		id   int
+		name string
+	)
+	rows, err := db.Query(`select id, name from account;`)
+	if err != nil {
+		t.Errorf("failed to select: %s", err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			t.Errorf("failed scan row: %s", err)
+		}
+		t.Logf("id: %d, name: %s", id, name)
+	}
+
 	err = dropTable(db)
 	if err != nil {
 		t.Errorf("failed to drop table: %s", err)
